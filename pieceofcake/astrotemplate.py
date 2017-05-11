19cc53ad9f8f347a2dd186815144d19c0db1901f
Julia Stadler <julia.j.stadler@durham.ac.uk>
2017-05-11 15:35:02 +0100

    astropytemplate works now

--- pieceofcake/astrotemplate.py
+++ pieceofcake/astrotemplate.py
@@ -3,8 +3,8 @@ import click
 @click.command()
 def setup_astropy_project():
 
-    redo = True
-    while(redo):
+    finish = False
+    while not (finish):
     
         package_name = click.prompt('Your package name')
 
@@ -29,17 +29,53 @@ def setup_astropy_project():
 
         project_url = click.prompt('Please give the project URL', default='', show_default=False)
 
-        example_code = click.prompt('include example code[Y/n]?', default=True, type=bool, show_default=False)
+        example_code = click.prompt('Do you want a folder for example code [Y/n]?', default=True, type=bool, show_default=False)
 
         include_cextern_folder = click.prompt('Do you want a folder for non-python C code [y/N]', default=False, show_default=False)
 
-    
-        click.echo('\nyour project:')
-        click.echo('package name: %s' %package_name)
-        click.echo('licence: %d' %licence)
-        click.echo('include example code: %s' %('yes' if example_code else 'no'))
+        project_version = click.prompt('What is the current version number of your project', default='0.0.dev')
 
-        redo = click.prompt('\nDo you want to change anything [y/n]', type=bool)
+        if (licence==1):
+            licence_str = 'BSD 3-Clause'
+        elif (licence==2):
+            licence_str = 'GNU GPL v3+'
+        elif (licence==3):
+            licence_str = 'Apache Software Licence 2.0'
+        elif (licence==4):
+            licence_str = 'BSD 2-Clause'
+        else:
+            licence_str = 'Other'
+    
+        click.echo('\nHere is the project you set up:')
+        click.echo('----------------------------------------')
+        click.echo('package name:\t\t%s' %package_name)
+        click.echo('module name:\t\t%s' %module_name)
+        click.echo('author name:\t\t%s' %author_name)
+        click.echo('author email:\t\t%s' %author_email)
+        click.echo('licence:\t\t%s' %licence_str)
+        click.echo('project version:\t%s' %project_version)
+        click.echo('project url:\t\t%s' %project_url)
+        click.echo('contains example code:\t%s' %('yes' if example_code else 'no'))
+        click.echo('contains C code:\t%s' %('yes' if include_cextern_folder else 'no'))
+        click.echo('short description:\t %s' %short_description)
+        click.echo('long description:\n\t%s' %long_description)
+        
+
+        finish = click.prompt('\nAre you happy with your project? [y/n]', type=bool)
+
+    project_values = { 'package name' : package_name,
+                       'module name' : module_name,
+                       'author name' : author_name,
+                       'author email' : author_email,
+                       'licence' : licence,
+                       'project version' : project_version,
+                       'project url' : project_url,
+                       'example code' : example_code,
+                       'cextern folder' : include_cextern_folder,
+                       'short description' : short_description,
+                       'long description' : long_description
+                       }
+    return project_values
     
 
     
